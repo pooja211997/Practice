6998a5c579a5eb6f0580a4cabd64e426e10e38e0
pooja211997 <167012357+pooja211997@users.noreply.github.com>
2024-05-29 18:10:38 -0500

    pro files

---
+++ letsTry/src/main/java/com/itexps/letstry/LetsTry.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.itexps.letstry;
+
+/**
+ *
+ * @author phalk
+ */
+public class LetsTry {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
